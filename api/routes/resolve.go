89326063884819cf582da15585af3967a7866220
yay14 <shreya.raj@razorpay.com>
2024-03-02 16:38:45 +0530

    init app

---
+++ api/routes/resolve.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/yay14/tiny-url-go/api/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, err := r.Get(database.ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short url not found in database" + err.Error(),
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to database" + err.Error(),
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.ctx, "counter")
+
+	return c.Redirect(val, 301)
+}
